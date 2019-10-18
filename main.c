2cd5386d9e87d314a8b0786aa947c993ce83aff4
NIKOLAOS MAKRYGEWRGOS <sdi1500238@linux01.di.uoa.gr>
2019-10-18 19:57:26 +0300

    pinakes

--- main.c
+++ main.c
@@ -6,8 +6,56 @@
 
 
 int main(int argc, char *argv[]){
+ //int
+ relation Rr_1, Rr_2;
+ relation Ss_1, Ss_2;
+
+ Rr_1.num_tuples = 100;	////
+ Rr_1.tuples = malloc(Rr_1.num_tuples * sizeof(tuple));
+ if(Rr_1.tuples == NULL){
+     printf("Error malloc Rr_1.tuples");
+     return -1;
+ }
+
+ Rr_2.num_tuples = 100; ////
+ Rr_2.tuples = malloc(Rr_2.num_tuples * sizeof(tuple));
+ if(Rr_2.tuples == NULL){
+     printf("Error malloc Rr_2.tuples");
+     return -1;
+ }
 
 
+
+ Ss_1.num_tuples = 100; ////
+ Ss_1.tuples = malloc(Ss_1.num_tuples * sizeof(tuple));
+ if(Ss_1.tuples == NULL){
+     printf("Error malloc Ss_1.tuples");
+     return -1;
+ }
+
+ Ss_2.num_tuples = 100; ////
+ Ss_2.tuples = malloc(Ss_2.num_tuples * sizeof(tuple));
+ if(Ss_2.tuples == NULL){
+     printf("Error malloc Ss_2.tuples");
+     return -1;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    free(Rr_1.tuples);
+    free(Rr_2.tuples);
+    free(Ss_1.tuples);
+    free(Ss_2.tuples);
 
     return 0;
 }
