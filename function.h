9b07b5e9dc5b8782a484274a26e82f16ff13b2ef
NIKOLAOS MAKRYGEWRGOS <sdi1500238@linux01.di.uoa.gr>
2019-10-20 22:57:29 +0300

    .

--- function.h
+++ function.h
@@ -25,5 +25,5 @@ void make_hist(relation **Rr_1, int r_size, int *hist, int hist_size);
 void make_p_sum(int *hist, int hist_size, int *p_sum, int p_sum_size);
 void make_Rr_2(relation **Rr_1, relation **Rr_2, int r_size, int *p_sum, int p_sum_size);
 
-void Sort_Merge_Join(relation **Rr, int r_size, relation **Ss, int s_size);
+void Sort_Merge_Join(relation **Rr, int r_size, relation **Ss, int s_size, info_deikti list);
 #endif
